3388d02164ff74fd98aebe89bc9179aaf91ba08f
RadimRiha <radim.rihacek@seznam.cz>
2021-04-14 16:18:30 +0200

    Completed

--- SQL scripts/FillExample.sql
+++ SQL scripts/FillExample.sql
@@ -3,16 +3,20 @@ INSERT INTO Subjects(Abbreviation, SubjectName) VALUES ('BPC-ALD', 'Algoritmy a 
 INSERT INTO Subjects(Abbreviation, SubjectName) VALUES ('BPC-VMP', 'Vektorový a maticový poèet')
 
 INSERT INTO Students(Firstname, Surname, DateOfBirth, ID) VALUES ('Tomáš', 'Hnidopich', '2000-06-02', 220010)
-INSERT INTO Students(Firstname, Surname, DateOfBirth, ID) VALUES ('Petr', 'Navrátil', '1998-07-05', 220011)
-INSERT INTO Students(Firstname, Surname, DateOfBirth, ID) VALUES ('Jan', 'Krabica', '1999-06-11', 220012)
+INSERT INTO Students(Firstname, Surname, DateOfBirth, ID) VALUES ('Petr', 'Novotný', '1998-07-05', 220011)
+INSERT INTO Students(Firstname, Surname, DateOfBirth, ID) VALUES ('Jan', 'Novotný', '1999-06-11', 220012)
 
 INSERT INTO Attendance(StudentID, SubjectAbbreviation) VALUES (220010, 'BPC-EL1');
 INSERT INTO Attendance(StudentID, SubjectAbbreviation) VALUES (220010, 'BPC-ALD');
 INSERT INTO Attendance(StudentID, SubjectAbbreviation) VALUES (220010, 'BPC-VMP');
 INSERT INTO Attendance(StudentID, SubjectAbbreviation) VALUES (220011, 'BPC-ALD');
+INSERT INTO Attendance(StudentID, SubjectAbbreviation) VALUES (220011, 'BPC-VMP');
 INSERT INTO Attendance(StudentID, SubjectAbbreviation) VALUES (220012, 'BPC-EL1');
-INSERT INTO Attendance(StudentID, SubjectAbbreviation) VALUES (220012, 'BPC-VMP');
+INSERT INTO Attendance(StudentID, SubjectAbbreviation) VALUES (220012, 'BPC-ALD');
 
-INSERT INTO Grading(StudentID, SubjectAbbreviation, GradingDate, Grade) VALUES (220010, 'BPC-VMP', '2021-04-10', 1);
-INSERT INTO Grading(StudentID, SubjectAbbreviation, GradingDate, Grade) VALUES (220010, 'BPC-EL1', '2021-04-12', 2);
-INSERT INTO Grading(StudentID, SubjectAbbreviation, GradingDate, Grade) VALUES (220012, 'BPC-VMP', '2021-04-13', 4);
+INSERT INTO Grading(StudentID, SubjectAbbreviation, GradingDate, Grade) VALUES (220010, 'BPC-EL1', '2021-04-10', 1);
+INSERT INTO Grading(StudentID, SubjectAbbreviation, GradingDate, Grade) VALUES (220010, 'BPC-ALD', '2021-04-10', 3);
+INSERT INTO Grading(StudentID, SubjectAbbreviation, GradingDate, Grade) VALUES (220010, 'BPC-VMP', '2021-04-12', 2);
+INSERT INTO Grading(StudentID, SubjectAbbreviation, GradingDate, Grade) VALUES (220011, 'BPC-ALD', '2021-04-13', 3);
+INSERT INTO Grading(StudentID, SubjectAbbreviation, GradingDate, Grade) VALUES (220011, 'BPC-VMP', '2021-04-15', 1);
+INSERT INTO Grading(StudentID, SubjectAbbreviation, GradingDate, Grade) VALUES (220012, 'BPC-ALD', '2021-04-13', 4);
---
+++ SQL scripts/SelectCommands.sql
@@ -0,0 +1,18 @@
+--all students and subjects
+SELECT Firstname, Surname, StudentID, SubjectAbbreviation 
+FROM Students LEFT JOIN Attendance ON Students.ID = Attendance.StudentID
+
+--number of students sharing a surname
+SELECT Surname, count(ID) AS Shared 
+FROM Students 
+GROUP BY Surname ORDER BY Shared DESC
+
+--subjects with less than 3 students
+SELECT SubjectAbbreviation 
+FROM Attendance 
+GROUP BY SubjectAbbreviation HAVING count(StudentID) < 3
+
+--best, worst and average grade
+SELECT SubjectAbbreviation, min(Grade) AS BestGrade, max(Grade) AS WorstGrade, avg(cast(Grade AS float)) AS AverageGrade, count(StudentID) AS GradedStudents
+FROM Grading 
+GROUP BY SubjectAbbreviation
